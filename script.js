304acc488205612080ecfca779b32555e4b7daa7
pauluspedrosa <paulus.pedrosa@outlook.com>
2023-07-10 20:32:06 +0900

    Create a function playRound
    This function takes the user's choice and the computer's choice as parameters
    and tells the user if the user has won against the computer.

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats Scissors.";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats Rock.";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats Scissors.";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beat Paper.";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beat Paper.";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock.";
+    } else if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+}
